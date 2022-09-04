28adb18d7c87811213829c90781355eba080c010
Olga Nadongo <winnienadongo@gmail.com>
2022-09-04 16:53:19 +0300

    commit tree 9a1f2ff0d93875cf37fbfdba7e4ada0a6242eb07

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+
+  get("/bakeries") do
+    Bakery.all.to_json
+
+  end 
+
+  get('/bakeries/:id') do
+    id = params[:id]
+    Bakery.find(id).to_json(include: :baked_goods)
+  end 
+
+  get('/baked_goods/by_price') do
+    baked_goods =  BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end 
+  
+  get('/baked_goods/most_expensive')do
+    most_expensive_good = BakedGood.where(price: BakedGood.maximum(:price)).first
+    most_expensive_good.to_json
+ end
 
 end
